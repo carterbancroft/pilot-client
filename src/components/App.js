4655c59faab292b52f4d2a05483e1c650c7dac22
carterbancroft <carter.bancroft@gmail.com>
2018-09-12 21:53:36 -0800

    Add some component framework as well as bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>App component!</p>
+            </div>
+        )
+    }
+}
+
+export default App
