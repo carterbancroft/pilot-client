a5207fb38c207022f448c2ec1a660580d0c89a8d
carterbancroft <carter.bancroft@gmail.com>
2018-09-12 22:31:38 -0800

    Testing making a request to the api and rendering the data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,47 @@
 import React from 'react'
 
+
+function Version(props) {
+    const { version } = props
+
+    if (!version.length) return <br />
+
+    return <div>{ version }</div>
+}
+
 class App extends React.Component {
+    constructor() {
+        super(...arguments)
+
+        this.state = {
+            version: ''
+        }
+
+        this.getData = this.getData.bind(this)
+    }
+
+    getData() {
+        fetch(
+            'http://localhost:9001/api/',
+            { credentials: 'same-origin' }
+        )
+            .then(res => res.json())
+            .then(json => {
+                this.setState({ version: json })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     render() {
+        const { version } = this.state
         return (
             <div>
-                <p>App component!</p>
+                <button onClick={ this.getData }>
+                    Get Some Data
+                </button>
+                <Version version={ version } />
             </div>
         )
     }
