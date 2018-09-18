73f7ab1bac4b40f1116a938cc7e83d9ce41d4aa1
carterbancroft <carter.bancroft@gmail.com>
2018-09-17 22:27:19 -0800

    Get basic posting of transactions working

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+const api_url = 'http://localhost:9001/api'
 
 function Transactions(props) {
     const { transactions } = props
@@ -26,6 +27,89 @@ function Transactions(props) {
     )
 }
 
+
+class TransactionForm extends React.Component {
+    constructor() {
+        super(...arguments)
+
+        this.state = {
+            item: '',
+            category: '',
+            amount_in_cents: '',
+            transaction_type: 'expense',
+        }
+
+        this.createTransaction = this.createTransaction.bind(this)
+        this.updateStateFromInput = this.updateStateFromInput.bind(this)
+    }
+
+    createTransaction() {
+        const user_name = 'carterbancroft'
+        console.log('create a transaction')
+
+        const url = `${api_url}/user/${user_name}/transaction`
+        console.log(url)
+        fetch(
+            url,
+            {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            },
+        )
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    updateStateFromInput(event) {
+        this.setState({ [event.target.name]: event.target.value })
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <form onSubmit={ (e) => e.preventDefault() }>
+                <input
+                    name="item"
+                    placeholder="Item"
+                    className="form-control"
+                    value={ this.state.item }
+                    onChange={ this.updateStateFromInput }
+                />
+                <input
+                    name="category"
+                    placeholder="Category"
+                    className="form-control"
+                    value={ this.state.category }
+                    onChange={ this.updateStateFromInput }
+                />
+                <input
+                    name="amount_in_cents"
+                    placeholder="0"
+                    className="form-control"
+                    value={ this.state.amount_in_cents }
+                    onChange={ this.updateStateFromInput }
+                />
+                <button
+                    className="btn btn-primary"
+                    onClick={ this.createTransaction }>
+                    Create Transaction
+                </button>
+            </form>
+        )
+    }
+}
+
+
 class App extends React.Component {
     constructor() {
         super(...arguments)
@@ -38,8 +122,10 @@ class App extends React.Component {
     }
 
     getData() {
+        const user_name = 'carterbancroft'
+        const url = `${api_url}/user/${user_name}/transaction`
         fetch(
-            'http://localhost:9001/api/user/carterbancroft/transaction',
+            url,
             {
                 method: 'GET',
                 credentials: 'same-origin'
@@ -58,6 +144,8 @@ class App extends React.Component {
         const { transactions } = this.state
         return (
             <div>
+                <TransactionForm />
+                <br />
                 <button className="btn btn-primary" onClick={ this.getData }>
                     Get Some Data
                 </button>
